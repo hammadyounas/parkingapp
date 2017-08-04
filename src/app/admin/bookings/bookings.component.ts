542900491acc5e21b2207b920240cd8606fd223b
hammadyounas <m.hammadyounas@outlook.com>
2017-08-04 17:33:06 +0500

    admin booking complete

--- src/app/admin/bookings/bookings.component.ts
+++ src/app/admin/bookings/bookings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from "../../provider/data.service";
 
 @Component({
   selector: 'bookings',
@@ -6,8 +7,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bookings.component.css']
 })
 export class BookingsComponent implements OnInit {
-
-  constructor() { }
+  obj: { placekey: any, userkey: any, bookingkey: any, address: any, date: any, endTime: any, reserveHour: any, slotNum: any, startTime: any };
+ arr = [];
+  constructor(
+    private _dataService: DataService,
+  ) { 
+    this.getbookings();
+  }
+  getbookings(){
+    this._dataService.Bookings().subscribe(user=>{
+      
+      this.arr = [];
+      user.forEach(element => {
+        console.log(element.key);
+        element.forEach(snapshot => {
+          console.log(snapshot.key);
+          snapshot.forEach(slot => {
+            this.obj = {placekey: {},userkey: {}, bookingkey: {} , address: {} ,
+             date: {} , endTime: {} , reserveHour: {}, slotNum: {} , startTime: {}};
+           this.obj.placekey = element.key;
+           this.obj.userkey = snapshot.key;
+           this.obj.bookingkey = slot.key;
+           this.obj.address = slot.val().address;
+           this.obj.date = slot.val().date;
+           this.obj.endTime = slot.val().endTime;
+           this.obj.reserveHour = slot.val().reserveHour;
+           this.obj.slotNum = slot.val().slotNum;
+           this.obj.startTime = slot.val().startTime;
+           this.arr.push(this.obj);
+          });
+        });
+      });
+      console.log(this.arr);
+    });
+  }
+  cancil(value: any){
+    console.log(value);
+    this._dataService.cancilBookings(value);
+    
+  }
 
   ngOnInit() {
   }
