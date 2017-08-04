acb2bb2dd45c088dc4260027b3ef5dfcbc1c1d9c
hammadyounas <m.hammadyounas@outlook.com>
2017-08-04 19:56:17 +0500

    admin user complete

--- src/app/admin/view-users/view-users.component.ts
+++ src/app/admin/view-users/view-users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from "../../provider/data.service";
 
 @Component({
   selector: 'view-users',
@@ -6,8 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-users.component.css']
 })
 export class ViewUsersComponent implements OnInit {
-
-  constructor() { }
+  arr = [];
+  obj: {userkey: any,Name: any, Email: any, Address: any, Number: any, uType: any};
+  constructor(
+    private _dataService: DataService
+  ) {
+    this.getData();
+   }
+  getData(){
+    this._dataService.users().subscribe(snapshots=>{
+      this.arr = [];
+      snapshots.forEach(element => {
+        console.log(element.key);
+        //console.log(element.val());
+        if(element.val().usertype != 'admin'){
+          this.obj = {userkey: {},Name: {}, Email: {}, Address: {}, Number: {}, uType: {}};
+          this.obj.userkey = element.key;
+          this.obj.Name = element.val().name;
+          this.obj.Email = element.val().email;
+          this.obj.Number = element.val().number;
+          this.obj.Address = element.val().address;
+          this.obj.uType = element.val().usertype;
+          this.arr.push(this.obj);
+          //console.log(this.arr);
+          
+        }
+      });
+    })
+  }
+  deleteuser(array){
+    ///console.log(array);
+    this._dataService.deleteUser(array.userkey);
+    
+  }
 
   ngOnInit() {
   }
--- src/app/provider/data.service.ts
+++ src/app/provider/data.service.ts
@@ -17,32 +17,41 @@ export class DataService {
   pushobj: FirebaseListObservable<any>;
   chat: FirebaseListObservable<any>;
   deleteBooking: FirebaseListObservable<any>;
- 
+
   uid;
   userType;
   slot = [
     'slot1', 'slot2', 'slot3', 'slot4', 'slot5', 'slot6', 'slot7', 'slot8', 'slot9', 'slot10', 'slot11',
     'slot12', 'slot13', 'slot14', 'slot15', 'slot16', 'slot17', 'slot18', 'slot19', 'slot20'
   ];
-  userData: FirebaseObjectObservable<any>
+  userData: FirebaseObjectObservable<any>;
+  userBooking: FirebaseListObservable<any>;
+  userChat: FirebaseListObservable<any>;
+  user: FirebaseObjectObservable<any>;
   constructor(
     private _authservice: AuthService,
     private db: AngularFireDatabase,
     private router: Router
   ) {
     this.uid = this._authservice.getUserId();
-    
+
   }
-  
- 
-  pushslot(placeName, tempdate, startTime, endTime, reservehour, slotnum ,placeAddress , userID) {
+  users(): Observable<any> {
+    this.userData = this.db.object('/userRegistration', { preserveSnapshot: true });
+    return this.userData.map(data => {
+      return data;
+    })
+  }
+
+
+  pushslot(placeName, tempdate, startTime, endTime, reservehour, slotnum, placeAddress, userID) {
     var obj = [
       {
         date: tempdate, startTime: startTime, endTime: endTime, reserveHour: reservehour, slotNum: slotnum
       }
     ];
     this.pushobj = this.db.list('/bookings/' + placeName + '/' + userID)
-    this.pushobj.push({ date: tempdate, startTime: startTime, endTime: endTime, reserveHour: reservehour, slotNum: slotnum , address: placeAddress }).then(data => {
+    this.pushobj.push({ date: tempdate, startTime: startTime, endTime: endTime, reserveHour: reservehour, slotNum: slotnum, address: placeAddress }).then(data => {
       console.log('slotbooked');
       alert('your parking has been booked :)');
       this.router.navigateByUrl('/user');
@@ -58,8 +67,8 @@ export class DataService {
       return slot
     })
   }
-  cancilBookings(value: any){
-    this.deleteBooking = this.db.list('/bookings/' + value.placekey + '/' + value.userkey,{preserveSnapshot: true});
+  cancilBookings(value: any) {
+    this.deleteBooking = this.db.list('/bookings/' + value.placekey + '/' + value.userkey, { preserveSnapshot: true });
     this.deleteBooking.remove(value.bookingkey);
   }
 
@@ -72,6 +81,30 @@ export class DataService {
       console.log(slot);
       return slot;
     });
+  }
+  deleteUser(key) {
+    this.user = this.db.object('/userRegistration/' + key, { preserveSnapshot: true });
+    this.user.remove();
+    for (var i = 0; i < 3; i++) {
+      if (i == 0) {
+        this.userBooking = this.db.list('/bookings/airport/', { preserveSnapshot: true });
+        this.userBooking.remove(key);
+      }
+      else if (i == 1) {
+        this.userBooking = this.db.list('/bookings/college/', { preserveSnapshot: true });
+        this.userBooking.remove(key);
+      }
+      else if (i == 2) {
+        this.userBooking = this.db.list('/bookings/hospital/', { preserveSnapshot: true });
+        this.userBooking.remove(key);
+      }
+
+    }
+    this.userChat = this.db.list('/chat/', { preserveSnapshot: true });
+    this.userChat.remove(key);
+
+
+    // })
   }
   getProfile(): Observable<any> {
     this.userData = this.db.object('/userRegistration/' + this.uid, { preserveSnapshot: true });
